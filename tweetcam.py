378aac71cf6dae920b06571e43ff593db6f9ca78
Kenneth Finnegan <kennethfinnegan2007@gmail.com>
2019-05-09 14:29:01 -0700

    Added timer mode on GPIO18

--- tweetcam.py
+++ tweetcam.py
@@ -18,18 +18,21 @@ from datetime import datetime
 
 GPIO.setmode(GPIO.BCM)
 
-yellow_led=22
-green_led=27
+# These are GPIO labels (BCM numbering) as opposed to header pin numbering
+record_led=22
+heartbeat_led=27
 shutter_pin = 17
+timer_enable = 18
 
 config = configparser.ConfigParser()
 config.read('/etc/tweetcam.conf')
 
-GPIO.setup (yellow_led, GPIO.OUT)
-GPIO.setup (green_led, GPIO.OUT)
+GPIO.setup (record_led, GPIO.OUT)
+GPIO.setup (heartbeat_led, GPIO.OUT)
 GPIO.setup (shutter_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.output(yellow_led, False)
-GPIO.output(green_led, False)
+GPIO.setup (timer_enable, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.output(record_led, False)
+GPIO.output(heartbeat_led, False)
 
 photo_taken=0
 
@@ -41,7 +44,20 @@ sleep(2)
 
 def take_photo(pin):
     global photo_taken
-    photo_taken += 1
+
+    # Check the timer switch to decide to sleep a little first
+    if (GPIO.input(timer_enable) == GPIO.LOW):
+        for x in range(3):
+            GPIO.output(record_led, True)
+            sleep(0.75)
+            GPIO.output(record_led, False)
+            sleep(0.75)
+        for x in range(3):
+            GPIO.output(record_led, True)
+            sleep(0.1)
+            GPIO.output(record_led, False)
+            sleep(0.1)
+        
 
     photoid = datetime.today().strftime('%Y%m%d%H%M%S') + "-" + str(random.randint(10000000,99999999)) + '.jpg'
     photodir = config['TWEETCAM']['photo_directory']
@@ -50,10 +66,12 @@ def take_photo(pin):
     # Take the actual photo and save it to our photo directory
     camera.capture(photo_filename)
 
+    GPIO.output(heartbeat_led, True)
+    photo_taken += 1
 
-    GPIO.output(yellow_led, True)
+    GPIO.output(record_led, True)
     send_email(photofile=photodir+photoid)
-    GPIO.output(yellow_led, False)
+    GPIO.output(record_led, False)
 
 
 def send_email(photofile=None):
@@ -81,22 +99,28 @@ def send_email(photofile=None):
 
 GPIO.add_event_detect(shutter_pin, GPIO.FALLING, callback=take_photo, bouncetime=200)
 
+heartbeat_pulse = 0
+
 while True:
-    GPIO.output(green_led, True)
-    sleep(0.05)
-    GPIO.output(green_led, False)
-    sleep(2)
+    heartbeat_pulse = (heartbeat_pulse + 1) % 25
+    if heartbeat_pulse == 0:
+        GPIO.output(heartbeat_led, True)
+        sleep(0.05)
+    else:
+        GPIO.output(heartbeat_led, False)
+        sleep(0.2)
     if photo_taken > 0:
         photo_taken -= 1
         for x in range(3):
-            GPIO.output(green_led, True)
+            GPIO.output(heartbeat_led, True)
             sleep(0.1)
-            GPIO.output(green_led, False)
+            GPIO.output(heartbeat_led, False)
             sleep(0.1)
 
 
 
 
 GPIO.cleanup(shutter_pin)
-GPIO.cleanup(yellow_led)
-GPIO.cleanup(green_led)
+GPIO.cleanup(timer_enable)
+GPIO.cleanup(record_led)
+GPIO.cleanup(heartbeat_led)
